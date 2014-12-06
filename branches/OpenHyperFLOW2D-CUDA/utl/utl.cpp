70a7beefb0d8121550c2916695fd4297906b325b
sergeas67@gmail.com <sergeas67@gmail.com@793415df-74b5-b2c0-507a-58b4c325f842>
2014-12-06 13:55:53 +0000

    minor fixes

--- branches/OpenHyperFLOW2D-CUDA/utl/utl.cpp
+++ branches/OpenHyperFLOW2D-CUDA/utl/utl.cpp
@@ -1,10 +1,10 @@
-/************************************************************
-*  UNKNOWN TEMPLATES LIBRARY (UTL) v1.3                     *
-*  Copyright (C) 1994-2010 Serge A. Suchkov                 *
-*  Copyright policy: LGPL V2.0                              *
-*  Matrix template.                                         *
-*  Please report all bugs and problems to "sergeas67@gmail.com". *
-************************************************************/
+/****************************************************************
+*  UNKNOWN TEMPLATES LIBRARY (UTL) v1.3                         *
+*  Copyright (C) 1994-2010 Serge A. Suchkov                     *
+*  Copyright policy: LGPL V2.0                                  *
+*  Matrix template.                                             *
+*  Please report all bugs and problems to "sergeas67@gmail.com".*
+*****************************************************************/
 #include "utl/umatrix2d.hpp"
 #ifdef _3D
 #include "utl/umatrix3d.hpp"
